09aa35f68f15bbbf4f32f74510e7b0883516fd92
Rauno <rauno.rander@mail.ee>
2025-09-13 14:16:28 +0300

    13.09

---
+++ extendingEventEmitter.mjs
@@ -0,0 +1,9 @@
+import EventEmitter from "event";
+
+class MyEmitter extends EventEmitter {}
+
+const myEmitter = new MyEmitter();
+myEmitter.on("event", () => {
+  console.log("An event occured");
+});
+myEmitter.emit("event");
---
+++ gettingFileStats.mjs
@@ -0,0 +1,19 @@
+import { promises as fs } from "fs";
+import path from "path";
+
+const __dirname = path.resolve();
+
+async function main() {
+  const files = await fs.readdir(__dirname, { withFileTypes: true });
+  for (const file of files) {
+    const stats = await fs.stat(path.join(__dirname, file.name));
+    console.log(
+      `${file.name} is ${stats.size} bytes and is ${
+        file.isDirectory() ? "a" : "not a"
+      } directory. It was created on ${stats.birthtime} and last modified on ${
+        stats.mtime
+      }`
+    );
+  }
+}
+main();
